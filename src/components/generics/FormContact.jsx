8aec4c12fa878816da16e543a540271ce4937ee1
GuillaumeLariviere <guillaume.lariviere59@gmail.com>
2022-02-09 11:30:46 +0100

    add fetch POST

--- src/components/generics/FormContact.jsx
+++ src/components/generics/FormContact.jsx
@@ -1,27 +1,27 @@
 // import { Link } from "react-router-dom";
 
 
-const FormContact =()=>{
-
+const FormContact =(props)=>{
+const {onClick} = props
     return (
-        <>
-<form>
-  <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" name="email" id="email" aria-describedby="emailHelp"/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label for="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" name="password" id="password"/>
-  </div>
-  <div className="mb-3">
-    <label for="exampleInputPassword1" className="form-label">Message</label>
-    <textarea className="form-control" name="formMessage" id="formMessage"rows="5"/>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-        </>
+<>
+    <form>
+        <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" name="email" id="email" aria-describedby="emailHelp"/>
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" name="password" id="password"/>
+        </div>
+        <div className="mb-3">
+            <label htmlFor="exampleInputmessage" className="form-label">Message</label>
+            <textarea className="form-control" name="formMessage" id="formMessage"rows="5"/>
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={onClick}>Submit</button>
+    </form>
+</>
     );
 };
 export default FormContact;
--- src/screens/ContactScreen.jsx
+++ src/screens/ContactScreen.jsx
@@ -1,14 +1,31 @@
+import { useEffect } from "react";
 import FormContact from "../components/generics/FormContact";
 
 
 
 
 const ContactScreen =() =>{
+   
 
+ function handleClick(evt){
+
+     evt.preventDefault();
+     const formEvt=evt.currentTarget.closest("form");
+     const formData = new FormData(formEvt)
+     const body = JSON.stringify(Object.fromEntries(formData.entries()));
+     const init = {
+        method: "post",
+        headers:{
+            "Content-Type": "application/json",
+        },
+        body
+    }
+    fetch("http://localhost:5000/contact",init).then(reps => reps.text()).then(console.log);
+ }
     return(
         <>
             <h1>ContactScreen</h1>
-            <FormContact/>
+            <FormContact onClick = {handleClick}/>
         </>
     );
 };
